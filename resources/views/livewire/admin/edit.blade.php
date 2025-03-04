cc84fc86d1eb076c71a3aa36c8f8b0413430ace8
Andres Crucitti <dasc495@gmail.com>
2025-03-03 17:36:37 -0800

    Initial commit

---
+++ resources/views/livewire/admin/edit.blade.php
@@ -0,0 +1,21 @@
+<?php
+
+use Livewire\Volt\Component;
+use Livewire\Attributes\Session;
+
+new class extends Component {
+
+    #[Session]
+    public $message = 'hello';
+
+    public function updateMessage()
+    {
+        $this->message = 'world';
+    }
+}
+
+?>
+    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+        {{ $message ?? '' }}
+        <flux:button wire:click="updateMessage">Update</flux:button>
+    </div>
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Volt::route('dashboard', 'admin.dashboard')->name('dashboard');
+    Volt::route('edit', 'admin.edit')->name('edit');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+});
+
+require __DIR__.'/auth.php';
