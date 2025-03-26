0400791fb08f64548e2bc577a18a134fc8030ff0
Andres Crucitti <dasc495@gmail.com>
2025-03-25 21:33:14 -0700

    add basic table component

---
+++ resources/views/components/table.blade.php
@@ -0,0 +1,12 @@
+@props([
+])
+
+<table class="w-full border-collapse">
+    <thead class="border-b border-gray-300">
+    {{ $head }}
+    </thead>
+
+    <tbody>
+    {{ $body }}
+    </tbody>
+</table>
---
+++ resources/views/components/table/th.blade.php
@@ -0,0 +1,6 @@
+@props([
+])
+
+<th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+    {{ $slot}}
+</th>
