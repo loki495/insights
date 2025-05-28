bcf8cfe0142b74e862556162c25fba4e98c92de0
Andres Crucitti <andres@ac495.net>
2025-05-28 00:52:06 -0700

    add parent to categories

--- database/migrations/2025_05_21_225844_create_categories_table.php
+++ database/migrations/2025_05_21_225844_create_categories_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('parent_id')->constrained();
             $table->string('name');
             $table->string('description')->nullable();
             $table->string('color')->nullable();
