be4c89c4d466dc90c40c52306ebc835f7b323d4d
Andres Crucitti <andres@ac495.net>
2026-02-25 15:58:25 -0800

    category and user model stuff

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -38,4 +38,37 @@ class Transaction extends Model
     {
         return $this->belongsToMany(Category::class)->withPivot('id');
     }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(Transaction::class, 'parent_id', 'id');
+    }
+
+     public function parent()
+    {
+        return $this->belongsTo(Transaction::class, 'parent_id');
+    }
+
+    public function scopeReportable($query)
+    {
+        $non_reportable_ids = Category::nonReportableIds();
+
+        return $query
+            ->where(function ($query) {
+                $query
+                    ->where(function ($q) {
+                        $q->whereNull('parent_id')
+                            ->where('is_split', false); // regular top-level
+                    })
+                    ->orWhereNotNull('parent_transaction_id'); // split children
+            //})
+            //->doesntHave('categories', function ($query) use ($non_reportable_ids) {
+                //$query->whereIn('id', $non_reportable_ids);
+            });
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
@@ -70,5 +71,10 @@ class User extends Authenticatable
     public function linkedAccounts(): HasMany
     {
         return $this->hasMany(LinkedAccount::class);
+    }
+
+    public function accounts(): HasManyThrough
+    {
+        return $this->hasManyThrough(Account::class, LinkedAccount::class);
     }
 }
